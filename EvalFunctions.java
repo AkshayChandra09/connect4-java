a8f06d26d7d299036face09489de68f26eb80e7b
Akshay Chandrachood <amc400@txstate.edu>
2017-11-25 11:26:51 -0600

    new evaluation function with comments

--- EvalFunctions.java
+++ EvalFunctions.java
@@ -5,16 +5,19 @@ public class EvalFunctions {
 	 
 	 Board current_board;
 	 int rows = 6;
-		int cols = 7;
+	 int cols = 7;
 	 int nodes=0;
+	 
+	 //called from each AI class
+	 //current status of board and function number are the arguments to this function
 	 
 	 public int evaluateBoard(char[][] b, int f_no){
 		 int score=0;
-		
-		 //score = function1(b);
 		 
-		 if(f_no==1){		
-			 
+		//call respective evaluation function which returns integer 
+		//value to represent goodness of Position from the standpoint of Player
+		 
+		 if(f_no==1){			 
 			 score = eval_function_one(b);
 		 }
 		 else{
@@ -24,249 +27,272 @@ public class EvalFunctions {
 		 return score;
 	 }
 	 
-	 int eval_function_one(char[][] b){
-		 int aiScore=1;
-	        int score=0;
-	        int blanks = 0;
-	        int k=0, moreMoves=0;
-	        for(int i=5;i>=0;--i){
-	            for(int j=0;j<=6;++j){
-	                
-	                if(b[i][j]==0 || b[i][j]==2) continue; 
-	                
-	                if(j<=3){ 
-	                    for(k=1;k<4;++k){
-	                        if(b[i][j+k]==1)aiScore++;
-	                 
-	                        else if(b[i][j+k]==2){aiScore=0;blanks = 0;break;}
-	                        else blanks++;
-	                    }
-	                     
-	                    moreMoves = 0; 
-	                    if(blanks>0) 
-	                        for(int c=1;c<4;++c){
-	                            int column = j+c;
-	                            for(int m=i; m<= 5;m++){
-	                             if(b[m][column]==0)moreMoves++;
-	                                else break;
-	                            } 
-	                        } 
-	                    
-	                    if(moreMoves!=0) score += calculateScore(aiScore, moreMoves);
-	                    //if(moreMoves!=0) score += calculateScore2(aiScore, moreMoves);
-	                    aiScore=1;   
-	                    blanks = 0;
-	                } 
-	                
-	                if(i>=3){
-	                    for(k=1;k<4;++k){
-	                        if(b[i-k][j]==1)aiScore++;
-	                        else if(b[i-k][j]==2){aiScore=0;break;} 
-	                    } 
-	                    moreMoves = 0; 
-	                    
-	                    if(aiScore>0){
-	                        int column = j;
-	                        for(int m=i-k+1; m<=i-1;m++){
-	                         if(b[m][column]==0)moreMoves++;
-	                            else break;
-	                        }  
-	                    }
-	                    if(moreMoves!=0) score += calculateScore(aiScore, moreMoves);
-	                    //if(moreMoves!=0) score += calculateScore2(aiScore, moreMoves);
-	                    aiScore=1;  
-	                    blanks = 0;
-	                }
-	                 
-	                if(j>=3){
-	                    for(k=1;k<4;++k){
-	                        if(b[i][j-k]==1)aiScore++;
-	                        else if(b[i][j-k]==2){aiScore=0; blanks=0;break;}
-	                        else blanks++;
-	                    }
-	                    moreMoves=0;
-	                    if(blanks>0) 
-	                        for(int c=1;c<4;++c){
-	                            int column = j- c;
-	                            for(int m=i; m<= 5;m++){
-	                             if(b[m][column]==0)moreMoves++;
-	                                else break;
-	                            } 
-	                        } 
-	                    
-	                    if(moreMoves!=0) score += calculateScore(aiScore, moreMoves);
-	                    //if(moreMoves!=0) score += calculateScore2(aiScore, moreMoves);
-	                    aiScore=1; 
-	                    blanks = 0;
-	                }
-	                 
-	                if(j<=3 && i>=3){
-	                    for(k=1;k<4;++k){
-	                        if(b[i-k][j+k]==1)aiScore++;
-	                        else if(b[i-k][j+k]==2){aiScore=0;blanks=0;break;}
-	                        else blanks++;                        
-	                    }
-	                    moreMoves=0;
-	                    if(blanks>0){
-	                        for(int c=1;c<4;++c){
-	                            int column = j+c, row = i-c;
-	                            for(int m=row;m<=5;++m){
-	                                if(b[m][column]==0)moreMoves++;
-	                                else if(b[m][column]==1);
-	                                else break;
-	                            }
-	                        } 
-	                        if(moreMoves!=0) score += calculateScore(aiScore, moreMoves);
-	                        //if(moreMoves!=0) score += calculateScore2(aiScore, moreMoves);
-	                        aiScore=1;
-	                        blanks = 0;
-	                    }
-	                }
-	                 
-	                if(i>=3 && j>=3){
-	                    for(k=1;k<4;++k){
-	                        if(b[i-k][j-k]==1)aiScore++;
-	                        else if(b[i-k][j-k]==2){aiScore=0;blanks=0;break;}
-	                        else blanks++;                        
-	                    }
-	                    moreMoves=0;
-	                    if(blanks>0){
-	                        for(int c=1;c<4;++c){
-	                            int column = j-c, row = i-c;
-	                            for(int m=row;m<=5;++m){
-	                                if(b[m][column]==0)moreMoves++;
-	                                else if(b[m][column]==1);
-	                                else break;
-	                            }
-	                        } 
-	                        if(moreMoves!=0) score += calculateScore(aiScore, moreMoves);
-	                        //if(moreMoves!=0) score += calculateScore2(aiScore, moreMoves);
-	                        aiScore=1;
-	                        blanks = 0;
-	                    }
-	                } 
-	            }
-	        }
-	        return score;
-	 }
 	 
-	 int calculateScore(int aiScore, int moreMoves){   
-	        int moveScore = 4 - moreMoves;
-	        if(aiScore==0)return 0;
-	        else if(aiScore==1)return 1*moveScore;
-	        else if(aiScore==2)return 10*moveScore;
-	        else if(aiScore==3)return 100*moveScore;
-	        else return 1000;
-	    }
+	//check for 4 consecutive occurrences of same player's disc
+	//or check if player win
 	 
-	 
-	/* public static int function2(char[][] b1){
-		 //Rows
-		 int row=0;
-		 int m=5;
-		 int count = 1;
-		 
-		 for(int i=0;i<7;i++){		 
-			 if(b1[row][i] == b1[row][i+1]){
-				 count++;
-			 }
-		 }
-		 
-		 //Columns
-		 
-		 System.out.println("Count is: "+ count);
-		 
-		 //left diagonal
-		 
-		 
-		 //Right diagonal
-		 
-		 return count;
-	 }
-	 */
-	 /*From connect4AI_MinMax*/
-	 
-		public boolean isGoal(char agent, char[][]board){
+	public boolean isGoal(char agent, char[][]board){
 			
+		//create string XXXX or 0000
+		String find=""+agent+""+agent+""+agent+""+agent;
 			
-			String find=""+agent+""+agent+""+agent+""+agent;
+		//checking 4 occurrences of player's disk in each row
+		for(int i=0; i<this.rows; i++)
+			if(String.valueOf(board[i]).contains(find))
+				return true;
 			
-			//check rows
-			for(int i=0; i<this.rows; i++)
-				if(String.valueOf(board[i]).contains(find))
-					return true;
+		//checking 4 occurrences of player's disk in each column
+		for(int j=0; j<this.cols; j++){
+			String col="";
+		for(int i=0; i<this.rows; i++)
+			col+=board[i][j];
 			
-			//check cols
-			for(int j=0; j<this.cols; j++){
-				String col="";
-				for(int i=0; i<this.rows; i++)
-					col+=board[i][j];
-					
-				if(col.contains(find))
-					return true;
-			}
+		//return true if string XXXX or 0000 found
+		if(col.contains(find))
+			return true;
+		}
 			
-			//check diags
-			ArrayList<Point> pos_right=new ArrayList<Point>();
-			ArrayList<Point> pos_left=new ArrayList<Point>();
+		
+		ArrayList<Point> pos_right=new ArrayList<Point>();
+		ArrayList<Point> pos_left=new ArrayList<Point>();
 			
-			for(int j=0; j<this.cols-4+1; j++)
-				pos_right.add(new Point(0,j));
-			for(int j=4-1; j<this.cols; j++)
-				pos_left.add(new Point(0,j));	
-			for(int i=1; i<this.rows-4+1; i++){
+		for(int j=0; j<this.cols-4+1; j++)
+			pos_right.add(new Point(0,j));
+		for(int j=4-1; j<this.cols; j++)
+			pos_left.add(new Point(0,j));	
+		for(int i=1; i<this.rows-4+1; i++){
 				pos_right.add(new Point(i,0));
-				pos_left.add(new Point(i,this.cols-1));
-			}
-		
-			//check right diags
-			for (Point p : pos_right) {
-				String d="";
-				int x=p.x, y=p.y;
-				while(true){				
-					if (x>=this.rows||y>=this.cols)
-						break;
-					d+=board[x][y];
-					x+=1; y+=1;
-				}
-				if(d.contains(find))
-					return true;
-			}
-			
-			//check left diags
-			for (Point p : pos_left) {
-				String d="";
-				int x=p.x, y=p.y;
-				while(true){
-					if(y<0||x>=this.rows||y>=this.cols)
-						break;
-					d+=board[x][y];
-					x+=1; y-=1;
-				}
-				if(d.contains(find))
-					return true;
-			}
-			
-			return false;
-			
+			pos_left.add(new Point(i,this.cols-1));
 		}
 		
+		//checking 4 occurrences of player's disk at each right diagonal
+		for (Point p : pos_right) {
+			String d="";
+			int x=p.x, y=p.y;
+			while(true){				
+				if (x>=this.rows||y>=this.cols)
+					break;
+				d+=board[x][y];
+				x+=1; y+=1;
+			}
+			if(d.contains(find))
+				return true;
+		}
+			
+		//checking 4 occurrences of player's disk at each left diagonal
+		for (Point p : pos_left) {
+			String d="";
+			int x=p.x, y=p.y;
+			while(true){
+				if(y<0||x>=this.rows||y>=this.cols)
+					break;
+				d+=board[x][y];
+				x+=1; y-=1;
+			}
+			if(d.contains(find))
+				return true;
+		}
 		
-
+		return false;	
+	}
 		
-		public int eval_function_two(char[][] board){    //evaluationFunction
+		
+    //First Evaluation Function - simple
+		
+	public int eval_function_one(char[][] board){   
 		 
-			if (isGoal('X',board)){   //0
-				return 1000;
-			}
-				
-			if (isGoal('0',board)){
-				return -1000;
-		     }
-				
-			return 0;
+		//check for XXXX, if found return 1000
+		if (isGoal('X',board)){   
+			return 1000;
 		}
+				
+		//check for 0000, if found return -1000
+		if (isGoal('0',board)){
+			return -1000;
+	    }
+				
+		return 0;
+	}
 		
-		/*Evaluation function extra*/
+	
+	
+	//Second Evaluation function - complex
+	//this evaluation function uses quadratic equation to get the value of the position
+	// euation: y = m^(x)+c 
+	//where y: value of the position
+	//      m: slope (any constant value) in this case it is assumed 5
+	//      c: any constant (to avoid returning value 0)
+	//      x: number of consecutive occurrences of player's disk
+	
+		
+	public static int eval_function_two(char[][] board){
+		int valueX=1;
+		int value0 = 1, value=0;
+		int score=1,scoreX_H=0, score0_H=0, scoreX_V=0, score0_V=0;
+		int scoreX_R=1, score0_R=1;
+		int m=5,c=10;
+		
+			
+		//horizontal checking for XXXX or 0000
+		for(int i=0;i<6;i++){
+			valueX=1; value0=1;
+			for(int j=0;j<6;j++){
+					
+				if(board[i][j]=='X' && board[i][j]==board[i][j+1] && valueX<4){
+					valueX++;
+					scoreX_H += (int)Math.pow(m, valueX)+c;
+					if(valueX==4){
+						return scoreX_H;
+					}
+				}
+					
+				else if(board[i][j]=='0' && board[i][j]==board[i][j+1] && value0<4){
+					value0++;
+					score0_H += (int)Math.pow(m, value0)+c;
+					score0_H*=-1;
+						
+					if(value0==4){
+						return score0_H;
+					}
+				}
+					
+				else if(board[i][j+1]==' '){
+					//score = 0;
+					continue;
+				}
+					
+				else if(board[i][j]!=board[i][j+1] && board[i][j+1]!=' '){
+					valueX=1; value0=1;
+					scoreX_H=0; score0_H=0;
+					continue;
+				}
+					
+			}
+			//System.out.println("\nHoriz Score0: "+score0_H);
+			score += ((valueX > value0) ? scoreX_H : score0_H);
+		}
+			
+		
+		//vertical checking for XXXX or 0000
+		
+		for(int j=0;j<7;j++){
+			valueX=1; value0 = 1;
+			for(int i=0;i<5;i++){
+							
+				if(board[i][j]=='X' && board[i][j]==board[i+1][j] && valueX<4){
+					valueX++;
+					scoreX_V += (int)Math.pow(m, valueX)+c;
+					if(valueX==4){
+						return scoreX_V;
+					}
+				}
+								
+				else if(board[i][j]=='0' && board[i][j]==board[i+1][j] && value0<4){
+					value0++;
+					score0_V -= (int)Math.pow(m, value0)+c;
+											
+					if(value0==4){
+						return score0_V;
+					}
+				}
+								
+				else if(board[i+1][j]==' '){
+					//score = 0;
+					continue;
+				}
+								
+				else if(board[i][j]!=board[i+1][j] && board[i+1][j]!=' '){
+					valueX=1; value0=1;
+					scoreX_V=0; score0_V=0;
+					continue;
+				}					
+							
+			}
+			//System.out.println("\nVerti Score0: "+score0_V);
+			score += ((valueX > value0) ? scoreX_V : score0_V);					
+		}
+			
+			
+	   //right diagonal checking for XXXX or 0000
+		
+		valueX=1; value0=1;
+			
+		for(int i =0; i < 3; i++){		 
+		     for(int j=0; j<4; j++){
+		           if(board[i][j]=='X' && board[i][j]==board[i+1][j+1] && valueX<4){
+		        	   valueX++;
+		            	scoreX_R+= (int)Math.pow(5, valueX)+10;
+		            	if(valueX==4){
+							return scoreX_R;
+						}
+		            }
+		            	
+		       else if(board[i][j]=='0' && board[i][j]==board[i+1][j+1] && value0<4){
+		    	   value0++;
+					score0_R -= (int)Math.pow(m, value0)+c;
+					if(value0==4){
+						return score0_R;
+						}
+					}
+		            	
+		        else if(board[i+1][j+1]==' '){
+					continue;
+				}
+						
+				else if(board[i][j]!=board[i+1][j+1] && board[i+1][j+1]!=' '){
+					valueX=1; value0=1;
+					scoreX_R=0; score0_R=0;
+					continue;
+				}
+		        
+		      //  score += ((valueX > value0) ? scoreX_R : score0_R);	
+		    }
+		     score += ((valueX > value0) ? scoreX_R : score0_R);	
+		 }
+
+
+		//System.out.println("\nTotal Score: "+score);
+			
+			
+		//left diagonal checking for XXXX or 0000
+		valueX=1; value0=1;
+		int scoreX_L=0, score0_L=0;
+		
+		for(int i=0; i<4; i++){		 
+		    for(int j=3; j>0; j--){
+		           if(board[i][j]=='X' && board[i][j]==board[i+1][j-1] && valueX<4){
+		        	   valueX++;
+		            	scoreX_L+= (int)Math.pow(5, valueX)+10;
+		            	if(valueX==4){
+							return scoreX_L;
+						}
+		           }
+		            	
+		           else if(board[i][j]=='0' && board[i][j]==board[i+1][j-1] && value0<4){
+		        	   value0++;
+					   score0_L -= (int)Math.pow(m, value0)+c;
+						if(value0==4){
+								return score0_L;
+						}
+					}
+		            	
+		            else if(board[i+1][j-1]==' '){
+						continue;
+					}
+						
+					
+		            else if(board[i][j]!=board[i+1][j+1] && board[i+1][j+1]!=' '){
+						valueX=1; value0=1;
+						scoreX_R=0; score0_R=0;
+						continue;
+					}   	
+		        }
+		       score += ((valueX > value0) ? scoreX_L : score0_L);
+		    }
+
+			//System.out.println("\nTotal Score: "+score);
+			
+			return score;
+		}
 		
 }
--- Interface.java
+++ Interface.java
@@ -9,26 +9,26 @@ public class Interface {
 	
 	public static void main(String[] args) throws CloneNotSupportedException{
 			
-		char [][]board = new char[][]{
-            {'X','X','0','X','X','X','0'},
-            {'X',' ','0',' ',' ',' ',' '},
-            {' ',' ','0',' ',' ',' ',' '},
-            {' ',' ','0',' ',' ',' ',' '},
+		/*char [][]board = new char[][]{
+            {'X','X','X','0','0','X','0'},
+            {'X','X','X','0',' ',' ',' '},
+            {' ','0','X','0',' ',' ',' '},
+            {' ',' ',' ','X',' ',' ',' '},
             {' ',' ',' ',' ',' ',' ',' '},
             {' ',' ',' ',' ',' ',' ',' '},    
-        };
+        };*/
         
-		//Scanner in = new Scanner(System.in);		
+		Scanner in = new Scanner(System.in);		
 		long startTime=0000;
 		//user_input();
-		System.out.println("Score main: "+eval_function_three(board));
+		//System.out.println("Score main: "+eval_function_three(board));
 		
-		/*System.out.println("\n1. MinMax Algorithm \t 2.Alpha-Beta Pruning \n");
+		System.out.println("\n1. MinMax Algorithm \t 2.Alpha-Beta Pruning \n");
 		int algo = in.nextInt();
 		System.out.println("\nPlease enter eval function no: (1/2): ");
-		eval = in.nextInt();*/
+		eval = in.nextInt();
 		
-		/*int nodes=0;
+		int nodes=0;
 		
 		try{
 			if(algo==1){
@@ -56,7 +56,7 @@ public class Interface {
 			}
 		}catch(Exception e){
 			 e.printStackTrace();
-		}*/
+		}
 	/*	long endTime   = System.currentTimeMillis();
 		long totalTime = endTime - startTime;
 		System.out.println("\nExecution Time: "+totalTime);*/
@@ -94,12 +94,15 @@ public class Interface {
 		System.out.println("\n.......Initialization............\n");
 	}
 	
-	public static int eval_function_three(char[][] board){
+	
+	
+	/*public static int eval_function_three(char[][] board){
 		int valueX=1;
 		int value0 = 1, value=0;
-		int score=1,scoreX_H=0, score0_H=0;
+		int score=1,scoreX_H=0, score0_H=0, scoreX_V=0, score0_V=0;
+		int scoreX_R=1, score0_R=1;
 		int m=5,c=10;
-		
+		ArrayList<Integer> row_arr = new ArrayList<Integer>();
 		
 		//horizontal checking
 		for(int i=0;i<6;i++){
@@ -109,7 +112,7 @@ public class Interface {
 				if(board[i][j]=='X' && board[i][j]==board[i][j+1] && valueX<4){
 					valueX++;
 					scoreX_H += (int)Math.pow(m, valueX)+c;
-					System.out.println("\nvalX: "+valueX+" score:"+scoreX_H);
+					//System.out.println("\nvalX: "+valueX+" score:"+scoreX_H);
 					if(valueX==4){
 						return scoreX_H;
 					}
@@ -119,7 +122,7 @@ public class Interface {
 					value0++;
 					score0_H += (int)Math.pow(m, value0)+c;
 					score0_H*=-1;
-					System.out.println("\nval0: "+value0+" score:"+score0_H);
+					//System.out.println("\nval0: "+value0+" score:"+score0_H);
 					if(value0==4){
 						return score0_H;
 					}
@@ -130,42 +133,48 @@ public class Interface {
 					continue;
 				}
 				
-				else if(board[i][j]!=board[i][j+1]){
-					valueX=1;
+				else if(board[i][j]!=board[i][j+1] && board[i][j+1]!=' '){
+					valueX=1; value0=1;
+					scoreX_H=0; score0_H=0;
 					continue;
 				}
 				
 			}
+
+			score += ((valueX > value0) ? scoreX_H : score0_H);
 		}
 		
-		System.out.println("\nHori ScoreX: "+valueX);
-		System.out.println("\nHori Score0: "+value0);
+		//System.out.println("\nHori ScoreX: "+scoreX_H);
+		//System.out.println("\nHori Score0: "+score0_H);
+		//System.out.println("\nTotal Score b4 vertical: "+score);
+	
 		
 		//vertical checking
-		/*valueX=1;
-		value0 = 1;
+		//valueX=1;
+		//value0 = 1;
 		
 				for(int j=0;j<7;j++){
+					valueX=1; value0 = 1;
 					for(int i=0;i<5;i++){
 						//if((board[i][j]=='X' && valueX==1) || board[i][j]!='X')
 							//continue;
 						
 						if(board[i][j]=='X' && board[i][j]==board[i+1][j] && valueX<4){
 							valueX++;
-							score = (int)Math.pow(m, valueX)+c;
-							System.out.println("\nvalX: "+valueX+" score:"+score);
+							scoreX_V += (int)Math.pow(m, valueX)+c;
+							//System.out.println("\nvalX: "+valueX+" score:"+scoreX_V);
 							if(valueX==4){
-								return score;
+								return scoreX_V;
 							}
 						}
 						
 						else if(board[i][j]=='0' && board[i][j]==board[i+1][j] && value0<4){
 							value0++;
-							score = (int)Math.pow(m, value0)+c;
-							score*=-1;
-							System.out.println("\nval0: "+value0+" score:"+score);
+							score0_V -= (int)Math.pow(m, value0)+c;
+							//score0_V*=-1;
+							//System.out.println("\nval0: "+value0+" score:"+score0_V);
 							if(value0==4){
-								return score;
+								return score0_V;
 							}
 						}
 						
@@ -174,18 +183,100 @@ public class Interface {
 							continue;
 						}
 						
-						else{
-							score = 0;
+						else if(board[i][j]!=board[i+1][j] && board[i+1][j]!=' '){
+							valueX=1; value0=1;
+							scoreX_V=0; score0_V=0;
 							continue;
-						}
-						
+						}					
 						
 					}
-				}*/
+					//System.out.println("\nVerti Score0: "+score0_V);
+				  score += ((valueX > value0) ? scoreX_V : score0_V);					
+			}
 		
-		System.out.println("\nHori ScoreX: "+scoreX_H);
-		System.out.println("\nHori Score0: "+score0_H);
+		//System.out.println("\nVerti ScoreX: "+scoreX_V);
+		//System.out.println("\nVerti Score0: "+score0_V);
+		System.out.println("\nTotal Score: "+score);
+		
+   	 //right diagonal checking
+	
+		valueX=1; value0=1;
+		
+		 for(int i =0; i < 3; i++){		 
+	            for(int j=0; j<4; j++){
+	            	if(board[i][j]=='X' && board[i][j]==board[i+1][j+1] && valueX<4){
+	            		valueX++;
+	            		scoreX_R+= (int)Math.pow(5, valueX)+10;
+	            		if(valueX==4){
+							return scoreX_R;
+						}
+	            	}
+	            	
+	            	else if(board[i][j]=='0' && board[i][j]==board[i+1][j+1] && value0<4){
+						value0++;
+						score0_R -= (int)Math.pow(m, value0)+c;
+						if(value0==4){
+							return score0_R;
+						}
+					}
+	            	
+	            	else if(board[i+1][j+1]==' '){
+						continue;
+					}
+					
+					else if(board[i][j]!=board[i+1][j+1] && board[i+1][j+1]!=' '){
+						valueX=1; value0=1;
+						scoreX_R=0; score0_R=0;
+						continue;
+					}
+	            score += ((valueX > value0) ? scoreX_R : score0_R);	
+	        }
+	    }
+		System.out.println("\nRight ScoreX: "+scoreX_R);
+		System.out.println("\nRight Score0: "+score0_R);
+		System.out.println("\nTotal Score: "+score);
+		
+		
+		//left diagonal checking
+		valueX=1; value0=1;
+		int scoreX_L=0, score0_L=0;
+		 for(int i=0; i<4; i++){		 
+	            for(int j=3; j>0; j--){
+	            	if(board[i][j]=='X' && board[i][j]==board[i+1][j-1] && valueX<4){
+	            		valueX++;
+	            		scoreX_L+= (int)Math.pow(5, valueX)+10;
+	            		if(valueX==4){
+							return scoreX_L;
+						}
+	            	}
+	            	
+	            	else if(board[i][j]=='0' && board[i][j]==board[i+1][j-1] && value0<4){
+						value0++;
+						score0_L -= (int)Math.pow(m, value0)+c;
+						System.out.println("\ni: "+i+" j: "+j+" val0: "+value0);
+						if(value0==4){
+							return score0_L;
+						}
+					}
+	            	
+	            	else if(board[i+1][j-1]==' '){
+						continue;
+					}
+					
+					else if(board[i][j]!=board[i+1][j+1] && board[i+1][j+1]!=' '){
+						valueX=1; value0=1;
+						scoreX_R=0; score0_R=0;
+						continue;
+					}
+	            score = ((valueX > value0) ? scoreX_L : score0_L);	
+	        }
+	    }
+		System.out.println("\nRight ScoreX: "+scoreX_L);
+		System.out.println("\nRight Score0: "+score0_L);
+		System.out.println("\nTotal Score: "+score);
+		
 		return score;
-	}
+	}*/
+	
 
 }
